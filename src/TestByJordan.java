c67f70af13b407ab78be404d0ff80c6457197a42
JordanOren <jordanoren@connect.glos.ac.uk>
2014-03-04 03:39:18 -0800

    This is also a comment for the testing

--- src/TestByJordan.java
+++ src/TestByJordan.java
@@ -3,6 +3,11 @@ public class TestByJordan {
 
 	public static void main(String[] args) {
 		System.out.println("hello Jordan");
+		
+		for(int i=0;i<100;i++){
+			System.out.println(i);
+			
+		}
 
 	}
 
