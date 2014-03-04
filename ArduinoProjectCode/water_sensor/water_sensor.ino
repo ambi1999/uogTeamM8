16788f02f02573a027f5fe72e21f2998356332a4
angeljayin <angeljayin@gmail.com>
2014-03-04 03:35:35 -0800

    working water sensor

---
+++ ArduinoProjectCode/water_sensor/water_sensor.ino
@@ -0,0 +1,13 @@
+const int analogInPin = A0;  
+int sensorValue = 0;        // value read from the pot
+
+void setup() {
+  Serial.begin(9600); 
+}
+
+void loop() {
+  sensorValue = analogRead(analogInPin);            
+  Serial.print("sensor = " );                       
+  Serial.print(sensorValue);      
+  delay(2);                     
+}
