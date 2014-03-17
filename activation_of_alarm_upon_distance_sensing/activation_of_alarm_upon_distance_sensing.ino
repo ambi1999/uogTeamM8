3edbf411805c1a31e56edf6fe11d4e530ed9f396
angeljayin <angeljayin@gmail.com>
2014-03-17 23:04:56 +0000

    alarm activation to prevent 3rd party accessing the arduino. Supports security and thus making the project innovative.

---
+++ activation_of_alarm_upon_distance_sensing/activation_of_alarm_upon_distance_sensing.ino
@@ -0,0 +1,33 @@
+#include <NewPing.h>
+
+int  TRIGGER_PIN = 11;
+int  ECHO_PIN    = 10;
+int MAX_DISTANCE = 200;  
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); 
+int DistanceCm;
+int  LED = 13;
+void setup()  
+{
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+
+}
+
+
+void loop()   
+{
+  delay(300);
+  DistanceCm = sonar.ping_cm();
+  Serial.print(DistanceCm); 
+  Serial.println("cm");
+ if (DistanceCm >= 0 && DistanceCm<= 50){
+ digitalWrite( LED, HIGH);
+ delay(500);
+ digitalWrite(LED, LOW);    
+ delay(500); 
+ }
+else {  digitalWrite(LED, LOW);}
+}
+
+
+
